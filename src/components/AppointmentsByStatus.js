a1c3b946394b62593f4786a4394a394e36124754
Bunlong Heng <bheng.code@gmail.com>
2025-05-28 16:15:56 -0400

    .env

--- src/components/AppointmentsByStatus.js
+++ src/components/AppointmentsByStatus.js
@@ -3,7 +3,7 @@ import axios from "axios";
 import { ResponsiveContainer, BarChart, Bar, XAxis, YAxis, CartesianGrid, Tooltip } from "recharts";
 
 const CUBE_API_URL = "http://localhost:4000/cubejs-api/v1/load";
-const CUBE_API_TOKEN = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpYXQiOjE3NDg0NjIyMjEsImV4cCI6MTc0OTA2NzAyMX0.xlbPRF4xO2pM4j_AqaQuZociRmFbYpi30vNRdmNoMcY";
+const CUBE_API_TOKEN = process.env.REACT_APP_CUBEJS_API_TOKEN;
 
 export default function AppointmentsByStatus() {
     const [data, setData] = useState([]);
